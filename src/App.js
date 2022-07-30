5bb45d2f0c47dac0d1866a46e5ee60030c444fcb
Monjul Boruah <monjulboruah@gmail.com>
2022-07-30 17:14:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,38 +10,50 @@ import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import CreateIncident from "./components/incident/CreateIncident";
+import Allincident from "./components/incident/AllIncident";
+import EditIncident from "./components/incident/EditIncident";
+import CreateZone from "./components/zone/CreateZone";
+import AllZones from "./components/zone/AllZones";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
 
   return (
     <div className={darkMode ? "app dark" : "app"}>
-       <DataProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={userInputs} title="Add New User" />}
-              />
+      <DataProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="all-incident" element={<Allincident />} />
+              <Route path="create-incident" element={<CreateIncident />} />
+              <Route path="edit-incident/:id" element={<EditIncident />} />
+              <Route path="create-zone" element={<CreateZone />} />
+              <Route path="all-zone" element={<AllZones />} />
+              <Route path="users">
+                <Route index element={<List />} />
+                <Route path=":userId" element={<Single />} />
+                <Route
+                  path="new"
+                  element={<New inputs={userInputs} title="Add New User" />}
+                />
+              </Route>
+              <Route path="products">
+                <Route index element={<List />} />
+                <Route path=":productId" element={<Single />} />
+                <Route
+                  path="new"
+                  element={
+                    <New inputs={productInputs} title="Add New Product" />
+                  }
+                />
+              </Route>
             </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route
-                path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
-              />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
       </DataProvider>
     </div>
   );
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from "axios"
+import {GlobalState} from "../../GlobalState"
 import {Link,useNavigate } from 'react-router-dom'
 import {useState,useContext} from 'react'
 import "./auth.scss"
@@ -6,30 +8,58 @@ import "./auth.scss"
 const Login = () => {
 
    
-    const [username,setUsername]= useState("");
-    const [password, setPassword] = useState("");
+    const state = useContext(GlobalState);
 
-    const onChangeUserName = (e) => {
-        setUsername(e.target.value)
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const onChangeEmail = (e) => {
+        setData({
+            ...data,
+            email: e.target.value
+        })
     }
     const onChangePwd = (e) => {
-        setPassword(e.target.value)
+        setData({
+            ...data,
+            password: e.target.value
+        })
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        let {email, password} = data;
+
+        try {
+            
+            let res = await  axios.post("http://localhost:5000/user/login", {
+                email,
+                password
+            })
+
+            console.log(res);
+            localStorage.setItem("loginStatus", true);
+            localStorage.setItem("role", res.data.role);
+            localStorage.setItem("userId", res.data.id);
+            localStorage.setItem("token", res.data.refreshtoken);
+            //window.location.href = "/dashboard";         
+        } catch (err) {
+            console.log(err);
+            //alert(err.response);
+        }
     }
    
-    console.log({
-        username, password
-    })
-   
-    
-    
    return (
         <form >
             <div  className="login-box">
                 <h1 >Login</h1>
-                <input type="text" placeholder="username" onChange={onChangeUserName} />
+                <input type="text" placeholder="username" onChange={onChangeEmail} />
                 <input type="password" placeholder="Password" onChange={onChangePwd}/>
                 <div>
-                    <button type="submit" className="login-button">Login</button>
+                    <button type="submit" className="login-button" onClick={onSubmit}>Login</button>
                 
                 </div>
                 <span>New to Upbelieve? <Link style={{color:"#1A94F1"}} to="/register">Register</Link></span>
